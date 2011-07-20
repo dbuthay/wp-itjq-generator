bbaf31ebed1229f82b6914fbc3a835eb8e22d4b6
Diego Buthay <dbuthay@gmail.com>
2011-07-20 11:33:19 -0700

    scripts for some themes may be precalculated .. speed boost!

--- app.rb
+++ app.rb
@@ -4,6 +4,8 @@ require 'sinatra'
 require 'wpitjq_generator'
 require 'haml'
 
+default_themes_dir = File.dirname(__FILE__) + "/public/defaults"
+
 get '/' do
   haml :index
 end
@@ -13,6 +15,15 @@ get '/generate' do
   halt "no feed" unless params[:feed]
   halt "no site" unless params[:site]
 
+  # check for precalculated themes .. easy way out!
+  theme = params[:theme] || ""
+  theme = theme.downcase.gsub(" ", "_") + ".js"
+
+  if theme && File.exists?("#{default_themes_dir}/#{theme}") then
+    return File.new "#{default_themes_dir}/#{theme}"
+  end
+
+  # else, calculate the js for this theme
   return Generator.new.for( params[:feed], params[:site] ) 
 
 end
